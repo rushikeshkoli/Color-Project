365bff84611f151cdbbdbd25a4ddf94fdbfad81a
rushikeshkoli <rushikeshkoli3@gmail.com>
2020-07-19 12:41:26 +0530

    Add Link to Palette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -45,12 +45,12 @@ const styles = {
 }
 
 function MiniPalette(props) {
-    const {classes, paletteName, emoji, colors} = props;
+    const {classes, paletteName, emoji, colors, handleClick} = props;
     const miniColorBoxes = colors.map(color => (
         <div className={classes.miniColor} style={{backgroundColor: color.color}} key={color.name} />
     ));
     return (
-        <div className={classes.root}>
+        <div className={classes.root} onClick={handleClick}>
             <div className={classes.colors} > {miniColorBoxes} </div>
             <h5 className={classes.title}>{paletteName}<span className={classes.emoji}>{emoji}</span></h5>
             
