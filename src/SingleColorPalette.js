381614d4770b3f3ddb60a616e3f64b3de86d1a22
rushikeshkoli <rushikeshkoli3@gmail.com>
2020-07-20 12:05:53 +0530

    Move out Styles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -4,47 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
+import styles from './styles/PaletteStyles';
 
-const styles = {
-    Palette: {
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        overflow: 'hidden',
-    },
-    colors: {
-        height: '90%'
-    },
-    goBack: {
-        height:'50%',
-        width: '20%',
-        margin: '0 auto',
-        display: 'inline-block',
-        position: 'relative',
-        cursor: 'pointer',
-        marginBottom: '-4px',
-        backgroundColor: 'black',
-        '& a': {
-            color: 'white',
-            width: '100px',
-            height: '30px',
-            position: 'absolute',
-            display: 'inline-block',
-            top: '50%',
-            left: '50%',
-            marginTop: '-15px',
-            marginLeft: '-50px',
-            outline: 'none',
-            fontSize: '1rem',
-            textAlign: 'center',
-            textTransform: 'uppercase',
-            lineHeight: '30px',
-            background: 'rgba(255, 255, 255, 0.3)',
-            border: 'none',
-            textDecoration: 'none',
-        }
-    }
-}
 
 class SingleColorPalette extends Component {
     constructor(props) {
---
+++ src/styles/PaletteStyles.js
@@ -0,0 +1,40 @@
+export default {
+    Palette: {
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'hidden',
+    },
+    colors: {
+        height: '90%'
+    },
+    goBack: {
+        height:'50%',
+        width: '20%',
+        margin: '0 auto',
+        display: 'inline-block',
+        position: 'relative',
+        cursor: 'pointer',
+        marginBottom: '-4px',
+        backgroundColor: 'black',
+        '& a': {
+            color: 'white',
+            width: '100px',
+            height: '30px',
+            position: 'absolute',
+            display: 'inline-block',
+            top: '50%',
+            left: '50%',
+            marginTop: '-15px',
+            marginLeft: '-50px',
+            outline: 'none',
+            fontSize: '1rem',
+            textAlign: 'center',
+            textTransform: 'uppercase',
+            lineHeight: '30px',
+            background: 'rgba(255, 255, 255, 0.3)',
+            border: 'none',
+            textDecoration: 'none',
+        }
+    }
+}
