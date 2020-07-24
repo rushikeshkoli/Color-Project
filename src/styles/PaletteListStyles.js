5336eee7f0ebac9224faa3b954a8419a3f41af02
rushikeshkoli <rushikeshkoli3@gmail.com>
2020-07-24 11:24:12 +0530

    Add backgroundSvg to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,12 +1,17 @@
 import sizes from "./sizes";
-
+import bg from "./bg.svg";
 export default {
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center",
+    overflow: "scroll",
+    backgroundColor: "#394abd",
+    backgroundImage: `url(${bg})`,
+  },
+  heading: {
+    fontSize: "2rem",
   },
   container: {
     width: "50%",
@@ -42,7 +47,7 @@ export default {
     },
     [sizes.down("xs")]: {
       gridTemplateColumns: "repeat(1, 100%)",
-      gridGap: "1rem",
+      gridGap: "1.4rem",
     },
   },
 };
